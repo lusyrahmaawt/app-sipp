13674d2f7e47f1056e823db2537dec5c20f81bb7
Febriansyah Riki Setiadi <febriansyahrikisetiadi@gmail.com>
2024-08-03 23:03:33 +0700

    fix bug to index3.html

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -209,7 +209,7 @@
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-3">
             <!-- Brand Logo -->
-            <a href="index3.html" class="brand-link">
+            <a href="{{ route('admin.dashboard') }}" class="brand-link">
                 <img src="{{ asset('assets/sibakul-ukm.jpg ') }}" class="brand-image img-circle elevation-3"
                     style="opacity: .8">
                 <span class="brand-text font-weight-light">SIPP</span>
